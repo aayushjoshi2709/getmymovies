22ba7183fe87557d90b846eeac3b5e5632d6d155
Aayush Joshi <48806014+jaayush27@users.noreply.github.com>
2021-07-11 20:44:41 +0530

    added search option code
    added code for searching pages and added some styling to cards and header

--- src/App.js
+++ src/App.js
@@ -3,48 +3,12 @@ import './App.css';
 import Header from './Components/Header';
 import ShowCards from './Components/ShowCards';
 import React,{ useEffect,useState } from 'react';
-const cards =[
-  {
-    name:'aayush',
-    rating:'3.8',
-    desc:'hi, there',
-    src:'https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/Ben10Omniverse.svg/375px-Ben10Omniverse.svg.png'
-  },
-  {
-    name:'sumit',
-    rating:'3.3',
-    desc:'hi, there how are you',
-    src:'https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/Ben10Omniverse.svg/375px-Ben10Omniverse.svg.png'
-  },
-  {
-    name:'neeraj',
-    rating:'3.8',
-    desc:'hi, guys',
-    src:'https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/Ben10Omniverse.svg/375px-Ben10Omniverse.svg.png'
-  },  {
-    name:'aayush',
-    rating:'3.8',
-    desc:'hi, there',
-    src:'https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/Ben10Omniverse.svg/375px-Ben10Omniverse.svg.png'
-  },
-  {
-    name:'sumit',
-    rating:'3.3',
-    desc:'hi, there how are you',
-    src:'https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/Ben10Omniverse.svg/375px-Ben10Omniverse.svg.png'
-  },
-  {
-    name:'neeraj',
-    rating:'3.8',
-    desc:'hi, guys',
-    src:'https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/Ben10Omniverse.svg/375px-Ben10Omniverse.svg.png'
-  }
-]
 const UPCOMMING_API = "https://api.themoviedb.org/3/movie/upcoming?api_key=20d913bda50836a3a7c9e7ad70acab65&language=en-US&page=1";
-const SEARCH_API ="" ;
+const SEARCH_API ="https://api.themoviedb.org/3/search/movie?api_key=20d913bda50836a3a7c9e7ad70acab65&language=en-US&page=1&include_adult=false&query=" ;
 
 function App() {
   const [movies,setMovies] = useState([]);
+  const [searchTerm,setSearchTerm] = useState([]);
   useEffect(() => {
     fetch(UPCOMMING_API)
     .then((res)=>res.json())
@@ -54,9 +18,25 @@ function App() {
       
     });
   }, [])
+  // submit the change form
+  const change = (e) =>
+  {
+    if(e.target.value !=='')
+    {
+      e.preventDefault();
+      fetch(SEARCH_API+e.target.value)
+      .then((res)=>res.json())
+      .then((data)=>{
+        console.log(data);
+        setMovies(data.results);
+      })
+    }else{
+      window.location.reload();
+    }
+  }
   return (
     <>
-   <Header isSearchBar={true}/>
+   <Header isSearchBar={true} handleOnChange={change}/>
    <ShowCards cards={movies}/>
    </>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,11 +3,11 @@ import './Header.css'
 import PropTypes from 'prop-types'
 import { BsHouseDoorFill } from "react-icons/bs";
 const Header = (props) => {
-    
     return (
         <header>
             {/*add the search box id isSearch bar is true else add the back button*/}
-            {props.isSearchBar?<input type='text' placeholder="Search"/>
+            {props.isSearchBar?
+                <input type='text' placeholder="Search" onChange = {props.handleOnChange}/>
             :<h2 className="movie-h2">Movie Details</h2>}
             <BsHouseDoorFill className="home-icon"/>
         </header>
